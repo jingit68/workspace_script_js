5512bfb358904c70c31f0b69ae6f094909df7e3f
jingit68 <jingit68@gmail.com>
2020-03-18 09:09:58 +1100

    this is real world start it now

---
+++ vim_lean/vim_normal_using.py
@@ -0,0 +1,60 @@
+insert  and  normal model:
+
+normal: is  ESC
+
+insert have:
+
+a append
+i insert
+o open a line below
+
+A append after line
+I insert before line
+O append a line above
+
+v select
+V select whole line
+y means copy select
+
+ctrl+v veiw block
+
+:q means quite
+:wq means write quite
+
+:set nu #get number line
+
+ctrl+H delete back space
+
+ctrl+W delete one word
+
+ctrl+u delet all line
+
+linux command ctrl+k
+
+linux comand ctrl + a to the front
+
+linux command ctrl + e to the end
+
+linux command ctrl + f move right
+
+linux comand ctrl + b move to lef
+
+
+:syntax on  # highlight
+
+in the normal w/W go next word
+
+in the noraml b/B go before word
+
+
+gg/G move to  header and end
+
+ctrl+o back to before
+
+H/M/L move to  head  middle and Lower
+
+Ctrl+u move to header
+
+ctrl+f move to forward
+
+
